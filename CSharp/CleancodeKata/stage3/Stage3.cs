255465ceed912c678797fc54792674160ac4b882
heidererkatrin <101861966+heidererkatrin@users.noreply.github.com>
2023-08-25 10:09:10 +0200

    Initial commit

---
+++ CSharp/CleancodeKata/stage3/Stage3.cs
@@ -0,0 +1,55 @@
+﻿namespace CleancodeKata.stage3
+{
+    /**
+     * Remove all the code duplications.
+     */
+    public static class Stage3
+    {
+        public static string DoTheThing(string input)
+        {
+            string result;
+            if (input.Length % 2 == 0)
+            {
+                result = input.Replace('A', 'a');
+                result = result.Replace('E', 'e');
+                var prefix = "even";
+                result = result.Replace('I', 'i');
+                result = result.Replace('O', 'o');
+                result = result.Replace('U', 'u');
+                result = result.Replace('Y', 'y');
+                if (result.Length < 10)
+                {
+                    result = prefix + "_" + result;
+                    result = "short" + "_" + result;
+                }
+                else
+                {
+                    result = prefix + "_" + result;
+                    result = "long" + "_" + result;
+                }
+            }
+            else
+            {
+                result = input.Replace('A', 'a');
+                var prefix = "odd";
+                result = result.Replace('E', 'e');
+                result = result.Replace('I', 'i');
+                result = result.Replace('O', 'o');
+                result = result.Replace('U', 'u');
+                result = result.Replace('Y', 'y');
+                if (result.Length < 10)
+                {
+                    result = prefix + "_" + result;
+                    result = "short_" + result;
+                }
+                else
+                {
+                    result = prefix + "_" + result;
+                    result = "long_" + result;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TypeScript/jest.config.ts
@@ -0,0 +1,14 @@
+import {pathsToModuleNameMapper} from "ts-jest/utils";
+
+const {compilerOptions} = require("./tsconfig");
+
+export default {
+    roots: ['<rootDir>/src', '<rootDir>/test'],
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageProvider: 'v8',
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {prefix: '<rootDir>/'}),
+};
